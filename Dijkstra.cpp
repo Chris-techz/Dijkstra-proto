f1a7cfc02ed8fb6fcae1ac5397bdc0fdf3401963
Chris-techz <christophegougis@gmail.com>
2021-06-16 10:03:54 +0200

    Dijkstra Fonctionnel

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -14,6 +14,7 @@ void Dijkstra::AfficherGrille()
 		} while (j < MAX);
 		std::cout << std::endl;
 	}
+	std::cout << std::endl;
 }
 
 Point Dijkstra::LireCases(Point actuel)
@@ -22,12 +23,12 @@ Point Dijkstra::LireCases(Point actuel)
 	c = { 9 };
 	int min = 9;
 
-	if (tic != 3 && actuel.x < MAX - 1) {
-		c.droite = tableau[actuel.y][actuel.x + 1];				//Lecture des cases adjacentes
-	}
-
 	if (tic != 4 && actuel.y < MAX - 1) {
 		c.bas = tableau[actuel.y + 1][actuel.x];
+	}
+
+	if (tic != 3 && actuel.x < MAX - 1) {
+		c.droite = tableau[actuel.y][actuel.x + 1];				//Lecture des cases adjacentes
 	}
 
 	if (actuel.x - 1 > 0 && tic != 1) {
@@ -39,25 +40,25 @@ Point Dijkstra::LireCases(Point actuel)
 	}
 
 	
-	if (min > c.droite && tic != 3) {									// Choix de la prochaine position
-		tic = 1;
-		min = c.droite;
-		prochainePosition = { actuel.y, actuel.x + 1 };
-	}
 	if (min > c.bas && tic != 4) {
 		tic = 2;
 		min = c.bas;
 		prochainePosition = { actuel.y + 1, actuel.x };
 	}
+	if (min > c.droite && tic != 3) {									// Choix de la prochaine position
+		tic = 1;
+		min = c.droite;
+		prochainePosition = { actuel.y, actuel.x + 1 };
+	}
 	if (min > c.gauche && tic != 1) {
 		tic = 3;
 		min = c.gauche;
-		prochainePosition = { actuel.y - 1, actuel.x };
+		prochainePosition = { actuel.y, actuel.x - 1};
 	}
 	if (min > c.haut && tic != 2) {
 		tic = 4;
 		min = c.haut;
-		prochainePosition = { actuel.y, actuel.x - 1 };
+		prochainePosition = { actuel.y - 1, actuel.x };
 	}
 
 	return prochainePosition;
@@ -81,6 +82,7 @@ void Dijkstra::ModifierPoid()
 
 void Dijkstra::Calcul()
 {
+	char entreeUser;
 	Point positionActuelle = { 0 };
 	Point positionSuivante = { 0 };
 	bool found = false;
@@ -89,15 +91,16 @@ void Dijkstra::Calcul()
 	{
 		positionSuivante = LireCases(positionActuelle);
 
-		if (positionSuivante.x == objectif.x && positionSuivante.y == objectif.y)
+		if (positionActuelle.x == objectif.x && positionActuelle.y == objectif.y) {
 			found = true;
-		else
+			std::cout <<  " Objectif atteint ! "<< std::endl << std::endl;
+		}
+		else {
 			positionActuelle = positionSuivante;
+			std::cout << "Faites une entree clavier pour avancer ..." << std::endl;
+			std::cin >> entreeUser;
+			std::cout << "X:" << positionActuelle.x << " ; Y:" << positionActuelle.y << std::endl << std::endl;
+		}
 
 	} while (found != true);
-}
-
-void Dijkstra::Avancer(Point suivant)
-{
-
 }
--- Dijkstra.h
+++ Dijkstra.h
@@ -4,17 +4,20 @@
 #include <stdio.h>
 #include <iostream>
 
-#define MAX 5						//Taille de la grille
+//Taille de la grille
+#define MAX 5	
 
-struct Case							//Case adjacentes de la position actuelle
+//Case adjacentes de la position actuelle
+struct Case							
 {
 	int droite	= 9, 
 		gauche	= 9, 
-		haut	= 9	, 
+		haut	= 9, 
 		bas		= 9;	
 };
 
-struct Point						//Coordonnées X, Y
+//Coordonnées X, Y
+struct Point						
 {
 	int y,
 		x;						
@@ -23,23 +26,26 @@ struct Point						//Coordonnées X, Y
 class Dijkstra
 {
 private:
-	Case c;					//initialisé au poid maximum
+	Case c;					
 	int tic = 0;
-	Point objectif = {2, 3};
-	int tableau[MAX][MAX] = {		//3 = case libre
-		{ 3, 3, 3, 3, 3 },			//9 = mur (poid max)
-		{ 9, 9, 9, 9, 3 },			//1 = objectif
-		{ 9, 9, 3, 9, 3 },			//Départ : X, Y = 0
-		{ 9, 3, 1, 3, 3 },
+	Point objectif = {3, 2};
+	int tableau[MAX][MAX] = {		
+		{ 3, 3, 3, 3, 3 },	//3 = case libre
+		{ 9, 9, 9, 9, 3 },	//9 = mur (poid max)
+		{ 9, 3, 9, 9, 3 },	//1 = objectif	
+		{ 9, 3, 1, 3, 3 },	//Départ : X, Y = 0
 		{ 3, 3, 3, 3, 9 }
 	};
 
 public:
-	void AfficherGrille();							//Affiche la grille en console
-	void Calcul();									//Dijkstra
-	Point LireCases(Point positionActuelle);		//Voir les noeuds suivant poid du trajet
-	void ModifierPoid();							//Modifie le poid en fonction des coordonnées de l'objectif
-	void Avancer(Point positionSuivante);									//Passe à la position suivante
+	//Affiche la grille en console
+	void AfficherGrille();		
+	//Dijkstra
+	void Calcul();				
+	//Voir le poid des cases adjacentes
+	Point LireCases(Point positionActuelle);
+	//Modifie le poid en fonction des coordonnées de l'objectif
+	void ModifierPoid();
 };
 
 #endif DIJKSTRA_H
